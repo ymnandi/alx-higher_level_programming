b909710ce0a20b6779842466f1b874155ffc781c
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-09-05 21:56:27 +0300

    whitespaces

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,4 +4,4 @@ for i in range(0, 100):
         print("{:02d}".format(i), end=", ")
     else:
         print("{:02d}".format(i))
-        
+    
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -8,4 +8,3 @@ def uppercase(str):
             i = chr(ord(i) - 32)
         print("{}".format(i), end="")
     print("")
-    
