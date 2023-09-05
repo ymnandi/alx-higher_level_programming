342afeb8ab748757d82a11f79b8e775228775f7e
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-09-05 09:50:41 +0300

    all

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Pyhton is an interpreted, interactive, object-oriented programming\
+    language that combines remarkable power with very clear syntax"
+str1 = str1[39:67] + str1[114:120] + str1[0:6]
+print(str1)
