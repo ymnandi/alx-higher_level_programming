d68986f308e95bb9d647b607ff9ffede5df54a4b
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-09-06 11:58:04 +0300

    101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 101-remove_char_at.py
+
+
+def remove_char_at(str, n):
+    """Create a copy of a string, removing the character at a position."""
+
+    if n < 0:
+        return str
+    return str[:n] + str[n + 1:]
