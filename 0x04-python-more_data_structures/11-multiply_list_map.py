cfcaaec6be6cda93ab29ab52d1279a493a68fa10
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-09-15 05:27:23 +0300

    all

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,25 +1,3 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-
-    if not isinstance(roman_string, str):
-        return 0
-
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-             'C': 100, 'D': 500, 'M': 1000}
-
-    num = 0
-    last_rom = 0
-
-    for ch in roman_string:
-        for r_num in rom_n.keys():
-            if r_num == ch:
-                if rom_n.get(ch) <= last_rom:
-                    num += rom_n.get(ch)
-                else:
-                    num += rom_n.get(ch) - last_rom * 2
-
-                last_rom = rom_n.get(ch)
-
-    return (num)
+def multiply_list_map(my_list=[], number=0):
+    return (list(map((lambda i: i * number), my_list)))
